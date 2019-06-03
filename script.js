017b076a88ccb7936f61122f565acf9f60822bcf
mjarraya <mjarraya@student.42.fr>
2019-06-03 15:27:17 +0200

    number type

--- script.js
+++ script.js
@@ -56,3 +56,75 @@ let j = k * 3;
 
 // exception for global constants
 const MAXIMUM_SIZE = 4800;
+
+// Number data type
+let mileage = 140000; // integer
+let engineVolume = 1.6; // floating point value
+
+// basic expression operators: +, -, *, /
+
+// addition
+let ageNextYear = 28 + 1;
+// age + 1
+console.log("next year: ", ageNextYear);
+
+// subtract
+let ageLastYear = 28 - 1;
+console.log("last year: ", ageLastYear);
+
+// multiply
+let ageTriple = age * 3;
+console.log("triple: ", ageTriple);
+
+// divide
+let halfAge = age / 2;
+console.log("half: ", halfAge);
+
+// modulo (useful to find multiples, e.g find if a number is odd or even)
+let evenRemainder = age % 2;
+console.log("even remainder: ", evenRemainder);
+
+let oddRemainder = 41 % 2;
+console.log("odd remainder: ", oddRemainder);
+
+// exponential
+// base ** exponent
+let tenPowerOf3 = 10 ** 3;
+
+// operator precedence is the same as in maths
+let randomNumber = 2 + 2 * 2; // 6
+let anotherRandomNumber = (2 + 2) * 2; // 8
+
+// assignment operators
+// addition
+let a = 1;
+// a = a + 1;
+a += 1;
+console.log("a: ", a); // 1
+// subtraction
+// a = a - 1;
+a -= 1;
+console.log("a: ", a); // 0
+// multiplication
+// a = a * 3;
+a *= 3;
+// divide
+a /= 2;
+// modulo
+a %= 2;
+// exponent
+a **= 10;
+let b = 9;
+b **= 0.5; // -> calculates the root
+// Math.sqrt(b) -> 3
+console.log(b); //3
+
+// const cannot be reassigned
+const c = 41;
+// c += 2; -> error
+const answerToLifeAndEverything = c + 1;
+console.log(c); // 41
+
+console.log(2 / 0); // Infinity
+const impossibleToCompute = "Abc" / 2;
+console.log(impossibleToCompute);
